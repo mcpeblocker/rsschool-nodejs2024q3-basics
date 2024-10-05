64dd4beb649eefda42bb7acabe4a91fcb0453aea
Alisher Ortiqov <mcpeblocker@gmail.com>
2024-10-05 15:18:08 +0900

    Subtasks completed:  - Command line interface(src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
+/**
+ * args.js
+ * - implement function that parses command line arguments
+ * (given in format
+ * --propName value --prop2Name value2
+ * , you don't need to validate it)
+ * and prints them to the console in the format
+ * propName is value, prop2Name is value2
+ */
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  let output = "";
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].slice(2);
+    const property = args[i + 1];
+    output += `${key} is ${property}, `;
+  }
+  output = output.trim().slice(0, output.length - 2);
+  console.log(output);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+/**
+ * env.js
+ * - implement function that parses environment variables
+ * with prefix RSS_ and prints them to the console
+ * in the format RSS_name1=value1; RSS_name2=value2
+ */
 const parseEnv = () => {
-    // Write your code here 
+  const envs = Object.keys(process.env).filter((e) => e.startsWith("RSS_"));
+  const output = envs
+    .reduce((output, env) => {
+      return output + `${env}=${process.env[env]}; `;
+    }, "")
+    .trim();
+  console.log(output);
 };
 
 parseEnv();
