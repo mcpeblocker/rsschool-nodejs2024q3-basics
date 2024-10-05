2a7d8af0759e109732ef03692a1bb31572a459da
Alisher Ortiqov <mcpeblocker@gmail.com>
2024-10-05 16:26:59 +0900

    Subtasks completed:  - Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import fsPromises from 'node:fs/promises';
+import { createHmac } from 'node:crypto';
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+    const content = await fsPromises.readFile(filePath, { encoding: "utf8" });
+    
+    const secret = 'abcdefg';
+    const hash = createHmac('sha256', secret).update(content).digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();
