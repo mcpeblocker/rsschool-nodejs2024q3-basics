ce13e50e087c77f2f8d8225dd43474e830581011
Alisher Ortiqov <mcpeblocker@gmail.com>
2024-10-05 18:55:47 +0900

    Subtasks completed:  - Zlib (src/zip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import zlib from "node:zlib";
+import stream from "node:stream/promises";
+import fs from "node:fs";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/**
+ * compress.js
+ * - implement function that compresses file fileToCompress.txt
+ * to archive.gz using zlib and Streams API
+ */
 const compress = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", "fileToCompress.txt");
+  const compressionPath = join(__dirname, "files", "archive.gz");
+  const sourceStream = fs.createReadStream(filePath);
+  const destinationStream = fs.createWriteStream(compressionPath);
+  const gzip = zlib.createGzip();
+
+  await stream.pipeline(sourceStream, gzip, destinationStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import zlib from "node:zlib";
+import stream from "node:stream/promises";
+import fs from "node:fs";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/**
+ * decompress.js
+ * - implement function that decompresses archive.gz
+ * back to the fileToCompress.txt with same content
+ * as before compression using zlib and Streams API
+ */
 const decompress = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", "fileToCompress.txt");
+  const compressionPath = join(__dirname, "files", "archive.gz");
+  const sourceStream = fs.createReadStream(compressionPath);
+  const destinationStream = fs.createWriteStream(filePath);
+  const unzip = zlib.createUnzip();
+
+  await stream.pipeline(sourceStream, unzip, destinationStream);
 };
 
 await decompress();
