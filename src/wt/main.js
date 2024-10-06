c6d39c19ba77d82daf2e13c9e631456c3748e39f
Alisher Ortiqov <mcpeblocker@gmail.com>
2024-10-06 12:50:40 +0900

    Subtasks completed:  - Worker Threads (src/wt)

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,70 @@
+/**
+ * main.js 
+ * - implement function that creates number of worker threads 
+ * (equal to the number of host machine logical CPU cores) 
+ * from file worker.js and able 
+ * to send data to those threads and 
+ * to receive result of the computation from them. 
+ * You should send incremental number starting from 10 to each worker. 
+ * For example: 
+ * on host machine with 4 cores you should create 4 workers and 
+ * send 10 to first worker, 
+ * 11 to second worker, 
+ * 12 to third worker, 
+ * 13 to fourth worker. 
+ * After all workers will finish, function should log array of results into console. 
+ * The results are array of objects with 2 properties:
+    status - 'resolved' in case of successfully received value from worker or 'error' in case of error in worker
+    data - value from worker in case of success or null in case of error in worker
+    
+ ! The results in the array must be in the same order that the workers were created
+ */
+
+import workers from "node:worker_threads";
+import os from "node:os";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpus = os.cpus();
+  const workerPath = join(__dirname, "worker.js");
+  const results = cpus.map((_, i) => ({
+    data: null,
+    resolve: null,
+  }));
+  const promises = results.map(
+    (_, i) =>
+      new Promise((resolve) => {
+        results[i].resolve = resolve;
+      })
+  );
+  for (const index in results) {
+    const n = parseInt(index) + 10;
+    const worker = new workers.Worker(workerPath, { workerData: n });
+    worker.once("message", (result) => {
+      results[index].data = result;
+    });
+    worker.once("exit", (exitCode) => {
+      const { resolve, data } = results[index];
+      if (!resolve) return;
+      if (exitCode === 1) {
+        // worker has been terminated with error
+        resolve({
+          status: "error",
+          data: null,
+        });
+      } else {
+        resolve({
+          status: "resolved",
+          data,
+        });
+      }
+    });
+  }
+  const finalResult = await Promise.all(promises);
+  console.log(finalResult);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,25 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+/**
+ * worker.js
+ * - extend given function
+ * to work with data received
+ * from main thread and
+ * implement function which
+ * sends result of the computation
+ * to the main thread
+ */
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+import workers from "node:worker_threads";
+
+// n should be received from main thread
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const n = workers.workerData;
+const result = nthFibonacci(n);
+
+const sendResult = (result) => {
+  // This function sends result of nthFibonacci computations to main thread
+  workers.parentPort.postMessage(result);
+  process.exit(0);
 };
 
-sendResult();
+sendResult(result);
