2c82a2779920afbd5213472ea4e6acdc2670ecd7
Alisher Ortiqov <mcpeblocker@gmail.com>
2024-10-06 15:34:03 +0900

    Subtasks completed:  - Child Processes (src/cp)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import cp from "node:child_process";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const spawnChildProcess = async (args = []) => {
+  const filePath = join(__dirname, "files", "script.js");
+  const child = cp.execFile("node", [filePath, ...args]);
+  child.stdout.on("data", (data) => {
+    console.log(`Child process:\n${data.toString()}`);
+  });
+  child.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+    process.exit(code);
+  });
+  process.stdin.on("data", (data) => {
+    child.stdin.write(data);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, "Hello, world!"]);
